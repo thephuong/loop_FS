5862687a1d078bd4d48345d31764ccf4ae8736ee
thephuong <thephuong@gmail.com>
2018-03-17 13:02:51 +0100

    correction func syntax

--- func_test_Perr_uni_cpx_MULTIFRAME.m
+++ func_test_Perr_uni_cpx_MULTIFRAME.m
@@ -1,4 +1,7 @@
-function func_test_Perr_uni_cpx_MULTIFRAME(N,rho_tot_dB,alpha,NBFRAME,NTEST)
+function func_test_Perr_uni_cpx_MULTIFRAME(N,rho_tot_dB,alpha,NBFRAME,NTEST,DO_SAVE)
+
+if nargin < 6; DO_SAVE=1; end
+
 % nopt = 90;mopt = 12;
 % N = nopt+mopt;
 % rho_tot_dB = -1;
@@ -13,6 +16,11 @@ sname = {'uni','ZC','bin'};
 %CONST
 CONST_ML_RULE=1;
 CONST_COR_RULE=2;
+
+SIMUPARAMS.NTEST = NTEST;
+SIMUPARAMS.min_NERR = 100;
+SIMUPARAMS.CONST_ML_RULE = 1;
+SIMUPARAMS.CONST_COR_RULE = 2;
 
 fprintf('TEST FS MULTIFRAMEs: instead of a single frame, use %d FRAMEs.\n', NBFRAME);
 
@@ -42,7 +50,7 @@ DO_VISUALIZE = 0;
 
 % cleanUp function
 fname_prefix = sprintf('tpn_MULTIFRAME%d_N%d_rho_tot%ddB_alphaPower%d_1e%d',NBFRAME,N,rho_tot_dB,alpha,-log10(epsilon));
-onExitObj = onCleanup(@()onExitFunc(fname_prefix));
+onExitObj = onCleanup(@()onExitFunc(DO_SAVE,fname_prefix));
 
 for im = 1:lenmm
 	m = mm(im);
@@ -64,13 +72,11 @@ for im = 1:lenmm
     perr2v2(im) = perr_uni_cpx_bloc(mv2,nv2,ssv2{im},rhoD_tabv2(im), CONST_ML_RULE, NTEST); %%%%%
 %     perr2v2_trunc(im) = perr_uni_cpx_bloc(2*m,2*n,ssv2{im}(1:end-1),N/(m*alpha+n)*rho_tot, CONST_ML_RULE, NTEST);
     %real Pe iterative ML rule
-    perr2_iterative1(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_ML_RULE, NTEST, 1,NBFRAME);
-%     perr2_iterative2(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_ML_RULE, NTEST, 2,NBFRAME);
-    perr2_iterative4(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_ML_RULE, NTEST, 4,NBFRAME);
+    perr2_iterative1(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_ML_RULE, 1,NBFRAME,SIMUPARAMS);
+    perr2_iterative4(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_ML_RULE, 4,NBFRAME,SIMUPARAMS);
     %real Pe iterative CORR rule
-    perr2_cor_iterative1(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_COR_RULE, NTEST, 1,NBFRAME);
-%     perr2_cor_iterative2(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_COR_RULE, NTEST, 2,NBFRAME);
-    perr2_cor_iterative4(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_COR_RULE, NTEST, 4,NBFRAME);
+    perr2_cor_iterative1(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_COR_RULE, 1,NBFRAME,SIMUPARAMS);
+    perr2_cor_iterative4(im) = perr_uni_cpx_iterative_bloc(m,n,ss{im},rhoD_tab(im), CONST_COR_RULE, 4,NBFRAME,SIMUPARAMS);
     fprintf('NBFRAME=%d (randi) m=%d n=%d: perr=%.3e perr2v2=%.3e (mv2=%d nv2=%d) perr2multiple=%.3e\n ===perr2_it_ML  %.3e | %.3e\n ===perr2_it_cor %.3e | %.3e\n', ...
         NBFRAME,m,n,perr(im),perr2v2(im),mv2,nv2,perr2(im), ...
         perr2_iterative1(im),perr2_iterative4(im), ...
@@ -95,12 +101,14 @@ if (DO_VISUALIZE)
 end
 
 %% Save
-function onExitFunc(fname_prefix)
-    dtt = datestr(datetime);
-    dtt(dtt==':')='-';
-    fname = sprintf('%s_%s.mat',fname_prefix,dtt);
-    fprintf('DONE or Interrupted. Saving data to %s\n', fname);
-    save(fname);
+function onExitFunc(DO_SAVE,fname_prefix)
+    if (DO_SAVE)
+        dtt = datestr(datetime);
+        dtt(dtt==':')='-';
+        fname = sprintf('%s_%s.mat',fname_prefix,dtt);
+        fprintf('DONE or Interrupted. Saving data to %s\n', fname);
+        save(fname);
+    end
 end
 
 end
