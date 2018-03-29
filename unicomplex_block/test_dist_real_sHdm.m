1e768997d74bd8d2c44ed3135cd15a6b421b59fc
AlexTP <thephuong@gmail.com>
2018-03-29 19:11:51 +0200

    update test distribution of sHdm

--- unicomplex_block/test_dist_real_sHdm.m
+++ unicomplex_block/test_dist_real_sHdm.m
@@ -3,6 +3,10 @@ clear all
 n = 20;
 m = randi(n);
 
+delt = randi(m);
+m1 = m-delt;
+m2 = delt;
+
 rhodB = 0;
 rho = 10^(rhodB/10);
 r = sqrt(n*rho);
@@ -19,17 +23,21 @@ histo = zeros(NTEST,1);
 histoo = zeros(NTEST,1);
 ndm = zeros(NTEST,1);
 for i = 1:NTEST
-	d = fGenUniVec_cpx(n,r);
-	dm = d(1:min(n,m));
+% 	d = fGenUniVec_cpx(n,r);
+%   dm = d(1:min(n,m));
+    d1 = fGenUniVec_cpx(n,r);
+    d2 = fGenUniVec_cpx(n,r);
+    dm = [d1(n-m2+1:n);d2(1:m1)];
+
 	ndm(i) = norm(dm);
 	histo(i) = real(s' * dm);
-    histoo(i) = real(d(1));
+%     histoo(i) = real(d(1));
 end
 
 DISP_FACTOR=1; HISTOBIN=40;
 figure;
 hh = histogram(histo,HISTOBIN);
-title(sprintf('Histogram of D_{0r}, n=%d, rho=%ddB',n,rhodB));
+title(sprintf('Histogram of D_{0r}, n=%d,m1=%d,m2=%d, rho=%ddB',n,m1,m2,rhodB));
 xx = hh.BinLimits(1):DISP_FACTOR*hh.BinWidth:hh.BinLimits(2);
 pxx = 1/norm(s)/sqrt(n*rho) * 1/beta(n-0.5,0.5) * (1-xx.^2/(norm(s)^2*n*rho)).^(n-1.5);
 hold on;
@@ -44,7 +52,7 @@ hh = histogram(histo,HISTOBIN,'Normalization','pdf');
 xx = hh.BinLimits(1):DISP_FACTOR*hh.BinWidth:hh.BinLimits(2);
 pxx = 1/norm(s)/sqrt(n*rho) * 1/beta(n-0.5,0.5) * (1-xx.^2/(norm(s)^2*n*rho)).^(n-1.5);
 pnx = normpdf(xx,0,norm(s)*sqrt(n*rho/(2*n-3)));
-title(sprintf('Histogram of D_{0r}, n=%d, rho=%ddB',n,rhodB));
+title(sprintf('Histogram of D_{0r}, n=%d,m1=%d,m2=%d, rho=%ddB',n,m1,m2,rhodB));
 hold on;
 plot(xx(1:2:end),pxx(1:2:end),'ro','LineWidth',1);
 plot(xx(2:2:end),pnx(2:2:end),'m+','LineWidth',2);
