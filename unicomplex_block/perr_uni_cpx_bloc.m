0b5454b25f1a4718d3bbfcb67f305b1ef1ae298e
thephuong <thephuong@gmail.com>
2018-04-01 18:35:16 +0200

    create ../unitfuncs/anonymous_func_selection.m

--- unicomplex_block/perr_uni_cpx_bloc.m
+++ unicomplex_block/perr_uni_cpx_bloc.m
@@ -8,30 +8,8 @@ min_NERR = SIMUPARAMS.min_NERR;
 N = m+n;
 r = sqrt(n*rho);
 
-switch (dataType)
-    case SIMUPARAMS.CONST_dataType_UNISPHERE
-        fGenData = @(n,r,ntest_perbloc) fGenUniVec_cpx_bloc(n,r,ntest_perbloc,-1);
-        switch (rule)
-            case SIMUPARAMS.CONST_ML_RULE
-                fmetric = @(y,tau,m,n,rho,s) fmetric_uni_cpx_bloc(circshift(y,-tau),m,n,rho,s);
-            case SIMUPARAMS.CONST_COR_RULE
-                fmetric = @(y,tau,m,n,rho,s) fmetric_cor_cpx_bloc(circshift(y,-tau),m,s);
-            otherwise
-                error('Rule(%d) not supported.',rule);
-        end
-    case SIMUPARAMS.CONST_dataType_GAUSSIAN
-        fGenData = @(n,r,ntest_perbloc) r/sqrt(2*n)*(randn(n,ntest_perbloc) + 1i*randn(n,ntest_perbloc));
-        switch (rule)
-            case SIMUPARAMS.CONST_ML_RULE
-                fmetric = @(y,tau,m,n,rho,s) fmetric_Normal_cpx_bloc(circshift(y,-tau),m,rho,s);
-            case SIMUPARAMS.CONST_COR_RULE
-                fmetric = @(y,tau,m,n,rho,s) fmetric_cor_cpx_bloc(circshift(y,-tau),m,s);
-            otherwise
-                error('Rule(%d) not supported.',rule);
-        end
-    otherwise
-        error('dataType(%d) not supported.',dataType);
-end
+% Select corresponding Data Generator and Decision metric
+[fmetric,fGenData] = anonymous_func_selection(rule,dataType,SIMUPARAMS);
 
 ntest_perbloc = 1000;
 nbloc = ceil(NTEST/ntest_perbloc);
---
+++ unitfuncs/anonymous_func_selection.m
@@ -0,0 +1,26 @@
+function [fmetric,fGenData] = anonymous_func_selection(rule,dataType,SIMUPARAMS)
+switch (dataType)
+    case SIMUPARAMS.CONST_dataType_UNISPHERE
+        fGenData = @(n,r,ntest_perbloc) fGenUniVec_cpx_bloc(n,r,ntest_perbloc,-1);
+        switch (rule)
+            case SIMUPARAMS.CONST_ML_RULE
+                fmetric = @(y,tau,m,n,rho,s) fmetric_uni_cpx_bloc(circshift(y,-tau),m,n,rho,s);
+            case SIMUPARAMS.CONST_COR_RULE
+                fmetric = @(y,tau,m,n,rho,s) fmetric_cor_cpx_bloc(circshift(y,-tau),m,s);
+            otherwise
+                error('Rule(%d) not supported.',rule);
+        end
+    case SIMUPARAMS.CONST_dataType_GAUSSIAN
+        fGenData = @(n,r,ntest_perbloc) r/sqrt(2*n)*(randn(n,ntest_perbloc) + 1i*randn(n,ntest_perbloc));
+        switch (rule)
+            case SIMUPARAMS.CONST_ML_RULE
+                fmetric = @(y,tau,m,n,rho,s) fmetric_Normal_cpx_bloc(circshift(y,-tau),m,rho,s);
+            case SIMUPARAMS.CONST_COR_RULE
+                fmetric = @(y,tau,m,n,rho,s) fmetric_cor_cpx_bloc(circshift(y,-tau),m,s);
+            otherwise
+                error('Rule(%d) not supported.',rule);
+        end
+    otherwise
+        error('dataType(%d) not supported.',dataType);
+end
+end
